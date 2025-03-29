af73838a6789c05cdde3e4ce3bcd9346b09f1a84
Andrii Unhurian <andrii.unhurian@sapient.pro>
2025-03-29 23:14:08 +0200

    chore: Refactor BaseApi module
    Consolidated the request methods in the BaseApi module for better clarity and organization. Ensure adherence to the 72-character limit for the subject line and each line of the body.

--- app/Api/BaseApi.php
+++ app/Api/BaseApi.php
@@ -4,20 +4,26 @@ declare(strict_types=1);
 
 namespace AUnhurian\NovaPoshta\SDK\Api;
 
+use AUnhurian\NovaPoshta\SDK\Exceptions\NovaPoshtaApiException;
+use AUnhurian\NovaPoshta\SDK\Exceptions\NovaPoshtaHttpException;
 use AUnhurian\NovaPoshta\SDK\Http\NovaPoshtaHttpClient;
 use AUnhurian\NovaPoshta\SDK\Http\NovaPoshtaResponse;
 
 /**
- * Base API class for all Nova Poshta API modules
+ * Base API module for Nova Poshta API
  */
 abstract class BaseApi
 {
     /**
+     * HTTP client for Nova Poshta API
+     *
      * @var NovaPoshtaHttpClient
      */
     protected NovaPoshtaHttpClient $httpClient;
 
     /**
+     * API model name for Nova Poshta API
+     *
      * @var string
      */
     protected string $modelName;
@@ -33,26 +39,30 @@ abstract class BaseApi
     }
 
     /**
-     * Send a request to Nova Poshta API
+     * Make a request to Nova Poshta API
      *
-     * @param string $method Method name
-     * @param array $properties Method properties
+     * @param string $calledMethod Method name
+     * @param array $methodProperties Method properties
      * @return array Response data
+     * @throws NovaPoshtaApiException
+     * @throws NovaPoshtaHttpException
      */
-    protected function request(string $method, array $properties = []): array
+    protected function request(string $calledMethod, array $methodProperties = []): array
     {
-        return $this->httpClient->request($this->modelName, $method, $properties);
+        return $this->httpClient->request($this->modelName, $calledMethod, $methodProperties);
     }
 
     /**
-     * Send a request to Nova Poshta API and get full response object
+     * Make a request to Nova Poshta API and get full response object
      *
-     * @param string $method Method name
-     * @param array $properties Method properties
+     * @param string $calledMethod Method name
+     * @param array $methodProperties Method properties
      * @return NovaPoshtaResponse Full response object
+     * @throws NovaPoshtaApiException
+     * @throws NovaPoshtaHttpException
      */
-    protected function requestWithFullResponse(string $method, array $properties = []): NovaPoshtaResponse
+    protected function requestWithFullResponse(string $calledMethod, array $methodProperties = []): NovaPoshtaResponse
     {
-        return $this->httpClient->requestWithFullResponse($this->modelName, $method, $properties);
+        return $this->httpClient->requestWithFullResponse($this->modelName, $calledMethod, $methodProperties);
     }
 }
